b7d080fbbaaf8d92e2ef71145b061dc1273e727d
Rockyroad <Racquelbryanteng@email.com>
2025-04-23 20:45:06 -0400

    changes to react app project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ WeatherData }) {
     return (
     <main>
         <WeatherCard />
@@ -8,7 +10,15 @@ function Main() {
         <p className="cards__text">
             Today is 75 &deg; F / You may want to wear:
             </p>
-            {/* TODO - add the cards */}
+            <ul className="cards__list">
+                {defaultClothingItems
+                //.filter((item) => {
+                    //return item.weather === WeatherData.type;
+                // })
+                 .map((item) => {
+                     return <ItemCard key={item._id} item={item} />;
+                })}
+            </ul>
     </section>
     </main>
     );
