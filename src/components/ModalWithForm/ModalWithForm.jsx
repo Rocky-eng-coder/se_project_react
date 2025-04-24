5e549c783d446ec6c226b4dce57ca24e46d40387
Rockyroad <Racquelbryanteng@email.com>
2025-04-24 18:04:49 -0400

    changes to react app project

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,13 +1,17 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title }) {
-return (<div className="modal">
+function ModalWithForm({ children, buttonText, title, activeModal, handleCloseClick }) {
+return (<div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
     <div className="modal__content">
     <h2 className="modal__title">{title}</h2>
-    <button type="button" className="modal__close">CLOSE</button>
+    <button 
+    onClick={handleCloseClick}
+    type="button" 
+    className="modal__close">CLOSE</button>
     <form className="Modal__form">
         { children }
-            <button type="submit" className="modal__submit">{buttonText}</button>
+            <button type="submit" 
+            className="modal__submit">{buttonText}</button>
     </form>
     </div>
 </div>
